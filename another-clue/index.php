c97b4b1cab386b655987b064cd3470814788414f
Parsa Hassani <hassani@chapman.edu>
2022-12-25 19:51:40 -0800

    New Website

---
+++ another-clue/index.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		 <style>
+		   #map {
+			height: 400px;
+			width: 100%;
+		   }
+		</style>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		<title>Another Clue!</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class='navbar-brand' href='index.php'>
+				It's Another Clue!
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+					<a href="index.php" class='nav-item nav-link'>Home</a>
+				</div>
+			</div>
+		</nav>
+		<br>
+		<br>
+		<div class="container-fluid">
+	<h2>It's Another Clue!</h2>
+	<h3>The Nicolas Cage Riddle Game</h3>
+	<p>
+		Welcome to It's Another Clue! This is a riddle game with the added benefit of being graced by Nicolas Cage's face!<br>
+		The objective of the game is simple. Solve five riddles in a row and win! However, if you get one wrong, you must start over!<br>
+		May Nicolas Cage be with you!
+	</p>
+	<img src="images/1.PNG" class="img-fluid" alt="Responsive image">
+	<br>
+	<br>
+	<a href='index.php?step=16' class='btn btn-info' role='button'>Start!</a>
+
+	</div>
+<div class='container-fluid'>
+	<p></p>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
+
